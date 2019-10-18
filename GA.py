6b43cc085918d27b4e2b8cc746e29522958f8c30
Vinícius Correa <viniciushenriquegc@unifei.edu.br>
2019-10-18 16:21:29 -0300

    coding the initial configuration of the GA

--- GA.py
+++ GA.py
@@ -1,2 +1,35 @@
 import FileHandle as fh
 
+class GA:
+    currentGen = 0 #Current Generation of the process
+    bestfitness = 0 #The fitness of the best individual
+    solvedMaze = [] #Maze after the GA Process
+
+    def __init__(self, mazeFile, numPopulation, crossoverIndex, crossoverMethod, mutationIndex, numGenerations):
+        self.mazeFile = mazeFile
+        self.numPopulation = numPopulation
+        self.crossoverIndex = crossoverIndex
+        self.crossoverMethod = crossoverMethod
+        self.mutationIndex = mutationIndex
+        self.numGenerations = numGenerations
+
+    def printConfigData(self):
+        print("Maze File: ",self.mazeFile,)
+        print("Nº Population: ",self.numPopulation,)
+        print("Crossover Index: ",self.crossoverIndex,)
+        if(self.crossoverMethod == 0):
+            print("Crossover Method: Roulette.")
+        else:
+            print("Crossover Method: Tournament.")
+        print("Mutation Index: ",self.mutationIndex,)
+        print("Nº Generations set: ",self.numGenerations,)      
+    
+    '''def beginGA():
+        if(self.crossoverMethod == 0):
+            beginRoulette()
+        else:
+            beginTournament()'''
+    
+    #def beginRoulette():
+
+    #def beginTournament():
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import GA
 import FileHandle as fh
 
 myFileHandle = fh.FileHandle()
-myFileHandle.readAndWriteExample("files/LabirintoExemplo01.txt","files/solvedLabirintoExemplo01.txt")
 
+#myFileHandle.readAndWriteExample("files/LabirintoExemplo01.txt","files/solvedLabirintoExemplo01.txt")
+
+newGA = GA.GA("files/LabirintoExemplo01.txt",2,56,0,13,27)
+newGA.printConfigData()
 
 #myfile = readFileWords("files/LabirintoExemplo01.txt")
 #beginGA("labirinth","population","Crossoverindex","mutationindex","ngenerations")
-#fitness, solvedlab = beginGA("labirinth","population","Crossoverindex","mutationindex","Ngeneration")

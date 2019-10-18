537583a08034c8e9dd856fa5dec59d032b66dab9
Vinícius Correa <viniciushenriquegc@unifei.edu.br>
2019-10-18 20:05:15 -0300

    Attribute individualSize created in order to alter the size of the individual. Now the size is the default individual size times the individualSize

--- GA.py
+++ GA.py
@@ -2,12 +2,14 @@ import FileHandle as fh
 
 class GA:
     currentGen = 0 #Current Generation of the process
+    individualDefaultSize = 0
     bestfitness = 0 #The fitness of the best individual
     solvedMaze = [] #Maze after the GA Process
 
-    def __init__(self, mazeFile, numPopulation, crossoverIndex, crossoverMethod, mutationIndex, numGenerations):
+    def __init__(self, mazeFile, numPopulation,individualSize, crossoverIndex, crossoverMethod, mutationIndex, numGenerations):
         self.mazeFile = mazeFile
         self.numPopulation = numPopulation
+        self.individualSize = individualSize
         self.crossoverIndex = crossoverIndex
         self.crossoverMethod = crossoverMethod
         self.mutationIndex = mutationIndex
@@ -16,6 +18,11 @@ class GA:
     def printConfigData(self):
         print("Maze File: ",self.mazeFile,)
         print("Nº Population: ",self.numPopulation,)
+        print("Individual Default size:",) #The size is the quantity of the blank spaces on the maze
+        if(self.numPopulation*self.individualDefaultSize > 0):
+            print("Individual size: ",self.numPopulation*self.individualDefaultSize)
+        else:
+            print("Individual size: Unset")
         print("Crossover Index: ",self.crossoverIndex,)
         if(self.crossoverMethod == 0):
             print("Crossover Method: Roulette.")
